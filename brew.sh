70c8cd461231955e1c50931d56190577193f1b93
Junji Zhi <jzhi316@gmail.com>
2018-05-26 08:44:41 -0400

    remove unnecessary options for setting up a mac for non-dev users

--- brew.sh
+++ brew.sh
@@ -40,40 +40,12 @@ brew install gnupg
 brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
-brew install screen
-brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
 brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -97,36 +69,17 @@ brew install zopfli
 
 # productivity apps
 brew cask install spectacle
-brew cask install --appdir="/Applications" evernote # https://gist.github.com/t-io/8255711
-brew cask install google-chrome
-brew cask install slack
 brew cask install vlc
 brew cask install flux
 brew cask install lastpass
 brew cask install keepassx
 brew cask install qvod-player
-brew cask install anki
-brew cask install android-file-transfer
 brew cask install time-out
 brew cask install xmind
-brew cask install macdown
 
 # security
 brew cask install gas-mask
 brew cask install malwarebytes
-
-# dev
-brew install the_silver_searcher
-brew cask install psequel
-brew cask install iterm2
-brew cask install dash
-
-# may need to run this twice after allowing kernel extensions
-brew cask install --appdir="/Applications" virtualbox
-
-brew install emacs --with-cocoa
-brew linkapps emacs
-
 
 # other apps recommended but not used. Can be enabled with uncommenting
 # brew cask install alfred # don't like it
