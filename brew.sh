73f163c5474b37bbd9c780f67ff9daa0b4f44bd9
Junji Zhi <jzhi316@gmail.com>
2021-04-17 09:26:35 -0400

    Update brew; Tune date and timezone

--- brew.sh
+++ brew.sh
@@ -21,8 +21,8 @@ brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
-brew install bash-completion2
+# brew install bash
+# brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
@@ -53,39 +53,40 @@ brew install ack
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
+# brew install pv
+# brew install rename
+# brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+# brew install vbindiff
+# brew install zopfli
+brew install markdown
 
 # ==== Beginning of extra apps ===
 
 # productivity apps
-brew cask install spectacle
-brew cask install vlc
-brew cask install flux
-brew cask install lastpass
-brew cask install keepassx
-brew cask install qvod-player
-brew cask install time-out
-brew cask install xmind
+brew install --cask spectacle
+brew install --cask vlc
+# brew install --cask flux
+# brew install --cask lastpass
+brew install --cask keepassx
+# brew install --cask qvod-player
+# brew install --cask time-out
+brew install --cask xmind
 
 # security
-brew cask install gas-mask
-brew cask install malwarebytes
+brew install --cask gas-mask
+# brew install --cask malwarebytes
 
 # other apps recommended but not used. Can be enabled with uncommenting
-# brew cask install alfred # don't like it
-# brew cask install --appdir="/Applications" wunderlist # NOT working
-# brew cask install electronic-wechat # not trusted its security
-# brew cask install cyberduck
+# brew install --cask alfred # don't like it
+# brew install --cask --appdir="/Applications" wunderlist # NOT working
+# brew install --cask electronic-wechat # not trusted its security
+# brew install --cask cyberduck
 
 # other apps to install through App store
 # wunderlist
